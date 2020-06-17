bb924e6778694fbdf90fb7c2dc75b25009c7dc91
Seth Malcolm <sethmal@microsoft.com>
2020-06-16 17:09:38 -0700

    Increased fogger time

--- tubman_single_test.py
+++ tubman_single_test.py
@@ -53,7 +53,7 @@ while True:
         if(humidity_tub2 < 90):
             GPIO.output(12,GPIO.LOW)
             print("Tub 2 Humid on")
-            time.sleep(8)  
+            time.sleep(20)  
                 ##Fan1 on
             GPIO.output(18,GPIO.LOW)
             time.sleep(2)
