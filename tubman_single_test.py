0081863d2c5237c1ebabbbc3e04514e33c06d089
Seth Malcolm <sethmal@microsoft.com>
2020-06-25 12:11:20 -0700

    more display changes

--- tubman_single_test.py
+++ tubman_single_test.py
@@ -138,7 +138,7 @@ while True:
 
     draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
     draw.text((x, top+8),     str(tempf_tub1), font=font, fill=255)
-    draw.text((x, top+16),    str(tempf_tub2), font=font, fill=255)
+    draw.text((x, top+16),    str(MemUsage), font=font, fill=255)
     draw.text((x, top+25),    str(Disk),  font=font, fill=255)
 
     # Display image.
