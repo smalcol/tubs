96e7b56ec86254500677f1c48b28f440d558944e
Seth Malcolm <sethmal@microsoft.com>
2020-06-25 12:07:59 -0700

    edit display

--- tubman_single_test.py
+++ tubman_single_test.py
@@ -137,8 +137,8 @@ while True:
     # Write four lines of text.
 
     draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     "Tub1 temp " + str(tempf_tub1), font=font, fill=255)
-    draw.text((x, top+16),    "Tub2 temp " + str(tempf_tub2), font=font, fill=255)
+    draw.text((x, top+8),     str(tempf_tub1), font=font, fill=255)
+    draw.text((x, top+16),    str(tempf_tub2), font=font, fill=255)
     draw.text((x, top+25),    str(Disk),  font=font, fill=255)
 
     # Display image.
